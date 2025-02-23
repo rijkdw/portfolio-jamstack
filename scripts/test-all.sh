ef75aabe3e5609f49f972b04ab5d4842b74d080d
Rijk de Wet <rijkdw@gmail.com>
2025-02-23 21:42:03 +0200

    maintenance: test script

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -4,13 +4,17 @@ function print-line() {
     echo "=============================================="
 }
 
-function test-one() {
+function run-test-command() {
     print-line
     echo "Testing $1:"
     print-line
     pnpm --filter $1 test
 }
 
-test-one ./packages/shared
-test-one ./packages/cms
-test-one ./packages/sanity-config
+for package in ./packages/*; do
+    run-test-command $package
+done
+
+for app in ./apps/*; do
+    run-test-command $app
+done
