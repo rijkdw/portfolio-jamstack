320afe23484db377478f3486ed96092ddbc1fe59
Rijk de Wet <rijkdw@gmail.com>
2025-03-03 10:04:23 +0200

    added sync-down util script

---
+++ packages/sanity-config/scripts/sync-down.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source "../../development.env"
+
+pnpm env-cmd -f ../../development.env pnpm sanity dataset export production production.tar.gz --overwrite
+
+if [ ! "$?" -eq 0 ]; then
+    echo "Failed to export dataset 'production'"
+    exit 1
+fi
+
+pnpm env-cmd -f ../../development.env pnpm sanity dataset delete development --force
+pnpm env-cmd -f ../../development.env pnpm sanity dataset create development --visibility public
+pnpm env-cmd -f ../../development.env pnpm sanity dataset import production.tar.gz development --replace
+
+if [ ! "$?" -eq 0 ]; then
+    echo "Failed to import dataset 'development'"
+    exit 1
+fi
